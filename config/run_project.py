6e62d1a3ce54f7925325fe8713b89da8363c9929
LuisGomes18 <luiscgomes0@gmail.com>
2024-10-19 03:50:04 +0100

    Update run_project def and the config.json

--- config/run_project.py
+++ config/run_project.py
@@ -14,13 +14,15 @@ def run_project(app):
     load_dotenv()
     config = load_config()
     status = config['status']
-    if not status:
-        raise ValueError('Not have status in config.json')
+    if not status or status not  in ['development', 'production']:
+        raise ValueError('Not have status or valid status in config.json')
 
     if status == 'development':
         PORT = getenv('DEVELOPMENT')
-    if status == 'production':
+    elif status == 'production':
         PORT = getenv('PRODUCTION')
+    else:
+        raise ValueError('Status not found in config.json')
     DEBUG = getenv('FLASK_DEBUG')
     HOST= getenv('FLASK_HOST')
     if not PORT or not DEBUG or not HOST:
