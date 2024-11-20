826762b35bd76b3fdf5cda85ca2b269c1c4d1f5e
Luis Gomes <luiscgomes0@gmail.com>
2024-11-20 16:23:58 +0000

    ✨ feat: function which verifes the authentication token

---
+++ app/extras/token_validation.py
@@ -0,0 +1,67 @@
+import logging
+import sys
+import os
+from dotenv import load_dotenv
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../..')
+from ..db.db import connect_database
+
+
+def token_validation(data, auth_header):
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info('\n\nToken Validation\n')
+    load_dotenv()
+    db = connect_database()
+    logging.info('Trying to acess the databse')
+    if not data:
+        logging.error('Data is not defined')
+        return {
+            'error': True,
+            'message': 'Data not defined',
+            'status_code': 400
+        }
+    if not db:
+        logging.error('Error trying to acess the databse')
+        return {
+            'error': True,
+            'message': 'Error trying to acess the databse',
+            'status_code': 500
+        }
+
+    if auth_header and auth_header.startswith('Bearer '):
+        try:
+            token = auth_header.split(' ')[1]
+            cursor = db.cursor()
+            query = 'SELECT * FROM Tokens WHERE token = %s'
+            cursor.execute(query, (token,))
+            logging.info('Execute the query in the database')
+            result = cursor.fetchone()
+            if not result:
+                logging.error('The token entered was not found in the database')
+                return {
+                    'error': True,
+                    'message': 'Token is invalid',
+                    'status_code': 401
+                }
+            else:
+                logging.info('The token entered was found in the databse')
+                return {
+                    'error': False,
+                    'message': 'Token is valid',
+                    'status_code': 201
+                }
+
+        except IndexError:
+            logging.error('The token entered is not valid format')
+            return {
+                'error': True,
+                'message': 'Not valid token format',
+                'status_code': 400
+            }
+    else:
+        logging.error('The token is not provided or invalid')
+        return {
+            'error': True,
+            'message': 'Token not provided or invalid',
+            'status_code': 401
+        }
