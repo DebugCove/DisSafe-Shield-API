6c13881aaef4f43196a8ec20e4fdad00b376a591
Luis Gomes <luiscgomes0@gmail.com>
2024-11-20 02:14:12 +0000

    🔥 fix: Removing old files
    Removing the old files to add the api using the template

--- test/url_transform.py
+++
@@ -1,9 +0,0 @@
-def url_transform(urls):
-    if isinstance(urls, list):
-        return urls
-
-    return urls.split()
-
-
-urls = "https://example.com https://test.com https://another_example.com"
-print(url_transform(urls)) 
--- test/uuid_generator.py
+++
@@ -1,8 +0,0 @@
-import uuid
-
-
-uuid_generated = uuid.uuid4()
-uuid_str = str(uuid_generated)
-
-print(f"Generated UUID: {uuid_str}")
-print(f"Number of characters: {len(uuid_str)}")
