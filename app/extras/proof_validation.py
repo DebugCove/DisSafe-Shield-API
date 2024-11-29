8c4f504a03ebc0239aedb0d8f15e6104cc89d8d6
Luis Gomes <luiscgomes0@gmail.com>
2024-11-29 18:08:25 +0000

    Fix code scanning alert - Full server-side request forgery #12

--- app/extras/proof_validation.py
+++ app/extras/proof_validation.py
@@ -1,24 +1,26 @@
 import logging
 import validators
 import requests
-
+from urllib.parse import urlparse
 
 def proof_validation(data):
     logging.basicConfig(level=logging.DEBUG)
     logging.info('\n\nProof Validation')
-    allowed_domains =  [
+
+    allowed_domains = [
         'imgur.com', 'flickr.com', 'photos.google.com', 'dropbox.com', 
         '500px.com', 'imageshack.com', 'photobucket.com', 
         'unsplash.com', 'onedrive.live.com', 'drive.google.com', 
         'icloud.com', 'mega.nz', '1drv.ms'
     ]
+
     success = []
     success_but = []
     fails = []
     invalid = []
     not_allowed_domains = []
 
-    proof = data['proof']
+    proof = data.get('proof')
     if not proof:
         return {
             'error': True,
@@ -36,9 +38,13 @@ def proof_validation(data):
             logging.error('Invalid URL: %s', url)
             invalid.append(url)
             continue
-        if not any(domain in url for domain in allowed_domains):
+
+        parsed_url = urlparse(url)
+        domain = parsed_url.netloc
+        if not any(domain.endswith(allowed_domain) for allowed_domain in allowed_domains):
             logging.error('URL: %s is not in allowed domains', url)
             not_allowed_domains.append(url)
+            continue
 
         try:
             response = requests.get(url, timeout=5)
@@ -53,7 +59,7 @@ def proof_validation(data):
     if fails or invalid or not_allowed_domains or success_but:
         return {
             'error': True,
-            'message': 'All the URLS have been checked, but there are some urls that are invalid or have problems or are not allowed.',
+            'message': 'All the URLs have been checked, but there are some URLs that are invalid, have problems, or are not allowed.',
             'status_code': 400,
             'data': {
                 'success': success,
@@ -63,11 +69,12 @@ def proof_validation(data):
                 'not_allowed_domains': not_allowed_domains,
             }
         }
+
     return {
-            'error': False,
-            'message': 'All URLS were checked without errors',
-            'status_code': 200,
-            'data': {
-                'success': success,
-            }
+        'error': False,
+        'message': 'All URLs were checked without errors',
+        'status_code': 200,
+        'data': {
+            'success': success,
+        }
     }
