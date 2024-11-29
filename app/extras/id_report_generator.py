de127f5fb2f5221d0b2d07643bf463144e7dba16
Luis Gomes <luiscgomes0@gmail.com>
2024-11-29 17:49:19 +0000

    Fix code scanning alert - Standard pseudo-random generators are not suitable for security/cryptographic purposes. #11 Fix code scanning alert - Standard pseudo-random generators are not suitable for security/cryptographic purposes. #10

--- app/extras/id_report_generator.py
+++ app/extras/id_report_generator.py
@@ -1,7 +1,7 @@
 import logging
 import sys
 import os
-from random import randint
+import uuid
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from db.db import connect_database
@@ -44,18 +44,20 @@ def report_id_generator():
         logging.info('Execute the query in the database')
         ids = cursor.fetchall()
 
-        new_id = randint(1111111111111111, 9999999999999999)
-        while new_id in [row[0] for row in ids]:
-            logging.warning('ID: %s already exists. Generating new one...', new_id)
-            new_id = randint(1111111111111111, 9999999999999999)
+        new_id = str(uuid.uuid4()).replace('-', '')
+        formated_new_id = new_id[:10]
+        while formated_new_id in [row[0] for row in ids]:
+            logging.warning('ID: %s already exists. Generating new one...', formated_new_id)
+            new_id = str(uuid.uuid4()).replace('-', '')
+            formated_new_id = new_id[:10]
 
-        logging.info('Generated new ID: %s', new_id)
+        logging.info('Generated new ID: %s', formated_new_id)
 
         return {
             'error': False,
             'message': 'New ID generated',
             'status_code': 201,
-            'data': new_id
+            'data': formated_new_id
         }
     except Exception as e:
         logging.error('Unexpected error: %s', e)
