58468a760879e78434264933d48af620a28f3bdd
Luis Gomes <luiscgomes0@gmail.com>
2024-12-04 03:28:06 +0000

    🐛 fix: Date correction to match database format

--- app/extras/info_generator.py
+++ app/extras/info_generator.py
@@ -2,7 +2,7 @@ from datetime import date, datetime
 
 
 def generate_date():
-    return date.today().strftime('%d/%m/%Y')
+    return date.today().strftime('%Y/%m/%d')
 
 def generate_hour():
     return datetime.now().strftime('%H:%M:%S')
