4801f49b350315bc83f0131172eaae29c63d0526
LuisGomes18 <luiscgomes0@gmail.com>
2025-01-05 23:44:35 +0000

    ✨ feat:  Addition of code to block CSRF and CORS attacks

--- app/app.py
+++ app/app.py
@@ -1,13 +1,27 @@
+import secrets
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 from app.initialize_functions import initialize_route, initialize_swagger
 from app.middleware.compression import compression
 
 
+csrf = CSRFProtect()
+
+
 def create_app() -> Flask:
     app = Flask(__name__)
+    app.secret_key = secrets.token_hex(24)
 
     initialize_route(app)
     initialize_swagger(app)
     compression(app)
+    csrf.init_app(app)
+
+    @app.after_request
+    def aplicar_cors(resposta):
+        resposta.headers["Access-Control-Allow-Origin"] = "https://discord.com"
+        resposta.headers["Access-Control-Allow-Methods"] = "GET, POST"
+        resposta.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        return resposta
 
     return app
