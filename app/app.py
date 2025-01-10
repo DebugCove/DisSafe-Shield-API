e608dc10d7f79c39a2c3799da3fe33b753d5ba0e
LuisGomes18 <luiscgomes0@gmail.com>
2025-01-10 21:43:21 +0000

    ♻️ refactor: Remove talisman from the code and update the security definition

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 import secrets
 from flask import Flask
-from flask_talisman import Talisman
 from app.initialize_functions import initialize_route, initialize_swagger
 from app.middleware.compression import compression
 
@@ -12,17 +11,17 @@ def create_app() -> Flask:
     initialize_route(app)
     initialize_swagger(app)
     compression(app)
-    Talisman(app)
 
 
     @app.after_request
     def security_settings(response):
-        response.headers['X-Content-Type-Options'] = 'nosniff'
-        response.headers['X-XSS-Protection'] = '1; mode=block'
-        response.headers['X-Frame-Options'] = 'DENY'
-        response.headers["Access-Control-Allow-Origin"] = "https://discord.com"
-        response.headers["Access-Control-Allow-Methods"] = "GET, POST"
-        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        response.headers['X-Content-Type-Options'] = 'nosniff' 
+        response.headers['X-XSS-Protection'] = '1; mode=block' 
+        response.headers['X-Frame-Options'] = 'DENY'  
+        response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"  
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"  
+        response.headers['Referrer-Policy'] = 'strict-origin-when-cross-origin' 
+
         return response
 
     return app
