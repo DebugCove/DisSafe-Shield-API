add57caf061fa41b0b52e4549fd8f53fb56a5a21
LuisGomes18 <luiscgomes0@gmail.com>
2025-05-08 22:32:17 +0100

    💄 feat:  Adding SSL and protection settings for django

--- DisSafe_Shield_API/settings.py
+++ DisSafe_Shield_API/settings.py
@@ -137,3 +137,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
