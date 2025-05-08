c42857bee09144d0ab528721ff52f7cf0c7fe8ac
LuisGomes18 <luiscgomes0@gmail.com>
2025-05-08 22:38:36 +0100

    ♻️ refactor: Improving the make_report route and adding the created definitions

--- v1_0/views.py
+++ v1_0/views.py
@@ -7,6 +7,7 @@ from .extras.info_generator import generate_date, generate_time
 from .extras.make_report.missing_data import missing_data
 from .extras.make_report.proof_validation import proof_validation
 from .extras.make_report.id_generator import id_generator
+from .extras.make_report.send_database import send_database
 
 
 @csrf_exempt
@@ -33,7 +34,11 @@ def status_view(request):
         return response
     elif request.method == 'GET':
         token = request.headers.get('Authorization')
-        auth_verf(token)
+        result = auth_verf(token, route='status ')
+        if result['error']:
+            if 400 <= result['status_code'] < 500:
+                return JsonResponse({'message': result['message']}, status=result['status_code'])
+            return JsonResponse({'message': 'Internal authentication error'}, status=500)
 
         return JsonResponse({'message': 'Status Ok'})
     else:
@@ -51,7 +56,6 @@ def make_report_view(request):
     if result['error']:
         if 400 <= result['status_code'] < 500:
             return JsonResponse({'message': result['message']}, status=result['status_code'])
-        print(result['message'])
         return JsonResponse({'message': 'Internal authentication error'}, status=500)
 
     try:
@@ -59,30 +63,34 @@ def make_report_view(request):
     except json.JSONDecodeError:
         return JsonResponse({'error': 'Invalid JSON'}, status=400)
     except Exception as e:
-        return JsonResponse({'error': f'Unexpected error: {str(e)}'}, status=400)
+        return JsonResponse({'message': 'Internal authentication error'}, status=500)
 
     result = missing_data(data)
     if result['error']:
         if 400 <= result['status_code'] < 500:
             return JsonResponse({'message': result['message']}, status=result['status_code'])
-        print(result['message'])
         return JsonResponse({'message': 'Internal authentication error'}, status=500)
 
     result = entry_validation(data)
     if result['error']:
         if 400 <= result['status_code'] < 500:
             return JsonResponse({'message': result['message']}, status=result['status_code'])
-        print(result['message'])
         return JsonResponse({'message': 'Internal authentication error'}, status=500)
 
     result = proof_validation(data)
     if result['error']:
         if 400 <= result['status_code'] < 500:
             return JsonResponse({'message': result['message']}, status=result['status_code'])
-        print(result['message'])
         return JsonResponse({'message': 'Internal authentication error'}, status=500)
+    data['proof'] = result['results']['successs']
 
     data['id'] = id_generator()
     data['date'] = generate_date()
     data['time'] = generate_time()
+
+    result = send_database(data)
+    if result['error']:
+        if 400 <= result['status_code'] < 500:
+            return JsonResponse({'message': result['message']}, status=result['status_code'])
+        return JsonResponse({'message': 'Internal authentication error'}, status=500)
     return JsonResponse({'message': 'Report created successfully'}, status=200)
